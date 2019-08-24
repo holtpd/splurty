dbd80e9cc0e12700e9b14bc1fda9a471470e70fe
holtpd <holt.patrick@gmail.com>
2019-08-24 18:31:26 +0000

    meta tag for mobile

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_14_004937) do
+ActiveRecord::Schema.define(version: 2019_08_24_145602) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2019_08_14_004937) do
     t.string "author"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "source_url"
   end
 
 end
